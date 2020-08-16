6422f1b920122601324de353212eda226e91ff6d
Matthew Harding <41506964+Acacia-Developer@users.noreply.github.com>
2020-08-16 22:26:14 +0100

    Added new scripts of "The Essence of Lerp" video

---
+++ The Essence of Lerp/Lerp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lerp : MonoBehaviour
+{
+    [SerializeField] private Transform a;
+    [SerializeField] private Transform b;
+    [SerializeField] private float t;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(a.position, b.position, t);
+    }
+}
---
+++ The Essence of Lerp/LerpColour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpColour : MonoBehaviour
+{
+    [SerializeField] private Color a;
+    [SerializeField] private Color b;
+    [SerializeField] private Color output;
+    [SerializeField][Range(0.0f, 1.0f)] private float t;
+
+    void Update()
+    {
+        output = Color.Lerp(a, b, t);
+    }
+}
